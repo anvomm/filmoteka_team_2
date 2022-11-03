df8089abb28f1494702b9b499e89c3b767a69823
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-11-04 01:10:26 +0200

    script for API requests done

---
+++ src/js/fetchMovies.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/';
+const API_KEY = 'd929b7a4b435aa22496bb0793b172bfc';
+
+//популярные фильмы на главную страницу
+export async function fetchTrendingMovies(page) {
+  try {
+    const { data } = await axios.get(
+      `${BASE_URL}3/trending/movie/day?api_key=${API_KEY}&page=${page}`
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//запрос по поисковому слову
+export async function fetchMovieByQuery(query, page) {
+  try {
+    const { data } = await axios.get(
+      `${BASE_URL}3/search/movie?api_key=${API_KEY}&query=${query}&page=${page}`
+    );
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//запрос по ид для модалки
+export async function fetchMovieById(id) {
+  try {
+    const { data } = await axios.get(
+      `${BASE_URL}3/movie/${id}?api_key=${API_KEY}`
+    );
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
