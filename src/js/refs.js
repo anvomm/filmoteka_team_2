d023e26f246fd38b3cb178bde7d1ffa4ae1b69b3
RuslanDakeshev <bil08@outlook.com>
2022-11-04 23:53:03 +0100

    search

--- src/js/refs.js
+++ src/js/refs.js
@@ -3,5 +3,8 @@ export default function refs() {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    form: document.querySelector('.search-form'),
+    formInput: document.querySelector('.form-input'),
+    notification: document.querySelector('.search-form__error-text'),
   };
 }
