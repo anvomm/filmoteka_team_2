d79b7061106937cd840bd18defa038f5b9b87a68
anvomm <104103036+anvomm@users.noreply.github.com>
2022-11-04 17:25:12 +0000

    Deploying to gh-pages from @ anvomm/filmoteka_team_2@36dd7b70e7fef636566e0653c564d411a498d24c 🚀

--- index.ed93569f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};!function(){function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}()}();
-//# sourceMappingURL=index.ed93569f.js.map
