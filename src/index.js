3015ca83840132f3c9968f0e75eb92a05a50f6c5
Nataly Siganova <siganova.tihanskaya@gmail.com>
2022-11-04 23:58:26 +0200

    add trending films

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './js/modal';
 import './js/uptop';
+import { fetchTrendingMovies } from './js/fetchMovies';
+import { renderList } from './js/renderFilmList';
+
+fetchTrendingMovies(1).then(data => {
+    if (data) {
+        renderList(data.results);
+    }
+});
