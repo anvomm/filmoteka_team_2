638f51ba60e9daeba55aea16878d8cc44993b755
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-11-04 09:40:23 +0200

    js files arranged

---
+++ src/js/modal.js
@@ -0,0 +1,12 @@
+import refsList from './refs';
+
+const refs = refsList();
+
+(() => {
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,7 @@
 export default function refs() {
-  return {};
+  return {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 }
