e267fda5c1a1e5c4f7a4c785b0b7e72849e0092f
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-11-05 11:30:55 +0200

    on esc and backdrop click close added

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,11 +2,28 @@ import refsList from './refs';
 
 const refs = refsList();
 
-(() => {
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.openModalBtn.addEventListener('click', onOpenModal);
+refs.closeModalBtn.addEventListener('click', onCloseModal);
+refs.backdrop.addEventListener('click', onCloseClickBackdrop);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+function onOpenModal() {
+  window.addEventListener('keydown', onCloseKeyEsc);
+  refs.modal.classList.remove('is-hidden');
+}
+
+function onCloseModal() {
+  window.removeEventListener('keydown', onCloseKeyEsc);
+  refs.modal.classList.add('is-hidden');
+}
+
+function onCloseClickBackdrop(e) {
+  if (e.currentTarget === e.target) {
+    onCloseModal();
   }
-})();
+}
+
+function onCloseKeyEsc(e) {
+  if (e.code === 'Escape') {
+    onCloseModal();
+  }
+}
--- src/js/refs.js
+++ src/js/refs.js
@@ -4,6 +4,7 @@ export default function refs() {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('.backdrop'),
     loader: document.querySelector('.loader'),
   };
 }
