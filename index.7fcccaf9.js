92a078802c83d5a53649e4b9d9306d3709edfaf0
anvomm <104103036+anvomm@users.noreply.github.com>
2022-11-06 16:19:17 +0000

    Deploying to gh-pages from @ anvomm/filmoteka_team_2@5089a07546c4806f4cbb06e4dcd70f732d8f7e01 🚀

---
+++ index.7fcccaf9.js
@@ -0,0 +1,2 @@
+new ldLoader({root:".ldld.full"}).on();
+//# sourceMappingURL=index.7fcccaf9.js.map
---
+++ index.fbe86873.js
@@ -0,0 +1,2 @@
+new ldLoader({root:".ldld.full"}).on();
+//# sourceMappingURL=index.fbe86873.js.map
