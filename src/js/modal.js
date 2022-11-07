34723b3e317c2431bd21ccd0ed8b812151e9985f
Yury Kusoits <kusoitsyury@gmail.com>
2022-11-07 23:36:30 +0100

    update modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,11 +2,18 @@ import refsList from './refs';
 import { fetchMovieById } from './fetchMovies';
 import modelTempl from '../templates/modal-content.hbs';
 const refs = refsList();
+const body = document.body;
 
 refs.closeModalBtn.addEventListener('click', onCloseModal);
 refs.backdrop.addEventListener('click', onCloseClickBackdrop);
 
 export function onOpenModal() {
+  let pagePosition = window.scrollY;
+  body.classList.add('disable-scroll');
+  body.dataset.position = pagePosition;
+  body.style.top = -pagePosition + 'px';
+  body.style.paddingRight = '17px';
+
   const id = this.dataset.action;
   fetchMovieById(id).then(resp => {
     const markup = modelTempl(resp);
@@ -18,6 +25,13 @@ export function onOpenModal() {
 }
 
 function onCloseModal() {
+  let pagePosition = parseInt(body.dataset.position, 10);
+  body.style.top = 'auto';
+  body.classList.remove('disable-scroll');
+  window.scroll({ top: pagePosition, left: 0 });
+  body.removeAttribute('data-position');
+  body.style.paddingRight = '0px';
+
   window.removeEventListener('keydown', onCloseKeyEsc);
   refs.modalTmp.innerHTML = '';
   refs.modal.classList.add('is-hidden');
