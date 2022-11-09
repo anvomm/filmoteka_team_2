da83dd3c825c8fa311e1e7b3279f93efd1b7d2ed
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-11-09 02:02:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import './js/themeSwitcher';
 import './js/localStorage';
 import './js/team';
 
-
 import './js/pagination';
+
+localStorage.clear();
 
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
