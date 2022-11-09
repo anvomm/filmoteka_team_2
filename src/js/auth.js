10444909f25af8c984f664a7f5b3a9890036fead
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-11-09 09:35:34 +0200

    validation correction

--- src/js/auth.js
+++ src/js/auth.js
@@ -9,44 +9,43 @@ const library = document.querySelector('.nav__item-auth');
 btnAuth.addEventListener('click', onOpenAuth);
 
 function onOpenAuth(e) {
-	if (btnAuth.textContent !== 'log out') {
-		backdrop.classList.remove('visually-hidden');
-	const form = document.querySelector('.auth__form');
-	form.addEventListener('submit', authFormHandler)
-	} else {
-		sessionStorage.removeItem('sign-in');
-		btnAuth.textContent = 'sign in';
-		library.classList.add('visually-hidden');
-		btnRecord.classList.remove('visually-hidden');
-	}
-	window.addEventListener("keydown", escapeKeyCloseModal);
-	backdrop.addEventListener("click", clickForCloseModal);
+  if (btnAuth.textContent !== 'log out') {
+    backdrop.classList.remove('visually-hidden');
+    const form = document.querySelector('.auth__form');
+    form.addEventListener('submit', authFormHandler);
+  } else {
+    sessionStorage.removeItem('sign-in');
+    btnAuth.textContent = 'sign in';
+    library.classList.add('visually-hidden');
+    btnRecord.classList.remove('visually-hidden');
+  }
+  window.addEventListener('keydown', escapeKeyCloseModal);
+  backdrop.addEventListener('click', clickForCloseModal);
 }
 
-function authFormHandler (e) {
+function authFormHandler(e) {
   e.preventDefault();
-  
-  const email = e.target.querySelector("#email-auth").value.trim();
-  const password = e.target.querySelector("#password-auth").value.trim();
 
-authWithEmailAndPassword(email, password).then(token => 
-    firebaseFetch(token)
-  ).then(renderModalAfterAuth)
-  }
+  const email = e.target.querySelector('.email-auth').value.trim();
+  const password = e.target.querySelector('.password-auth').value.trim();
 
-  function escapeKeyCloseModal(event) {
-    if (event.code === "Escape") {
-      backdrop.classList.add('visually-hidden');
-    }
-  }
-  
-  function clickForCloseModal(event) {
-    if (event.target === event.currentTarget) {
-      backdrop.classList.add('visually-hidden');
-    }
-  }
+  authWithEmailAndPassword(email, password)
+    .then(token => firebaseFetch(token))
+    .then(renderModalAfterAuth);
+}
 
-  
+function escapeKeyCloseModal(event) {
+  if (event.code === 'Escape') {
+    backdrop.classList.add('visually-hidden');
+  }
+}
+
+function clickForCloseModal(event) {
+  if (event.target === event.currentTarget) {
+    backdrop.classList.add('visually-hidden');
+  }
+}
+
 function authWithEmailAndPassword(email, password) {
   return fetch(
     `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${firebaseConfig.apiKey}`,
@@ -83,15 +82,15 @@ function firebaseFetch(token) {
 }
 
 function renderModalAfterAuth(content) {
-	if (typeof content === 'string') {
-		error.textContent = 'INVALID EMAIL OR PASSWORD';
-	} else {
-		error.textContent = '';
-		backdrop.classList.add('visually-hidden');
-		library.classList.remove('visually-hidden');
-		btnAuth.textContent = 'log out'
-		sessionStorage.setItem('sign-in', 'true');
-		btnRecord.classList.add('visually-hidden');
-	}
-	console.log(content);
+  if (typeof content === 'string') {
+    error.textContent = 'INVALID EMAIL OR PASSWORD';
+  } else {
+    error.textContent = '';
+    backdrop.classList.add('visually-hidden');
+    library.classList.remove('visually-hidden');
+    btnAuth.textContent = 'log out';
+    sessionStorage.setItem('sign-in', 'true');
+    btnRecord.classList.add('visually-hidden');
+  }
+  console.log(content);
 }
