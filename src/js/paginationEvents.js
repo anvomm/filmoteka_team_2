92af5a98fed10a70e9cdf117ca4be73f29d7bb49
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-11-10 21:21:15 +0200

    Update paginationEvents.js

--- src/js/paginationEvents.js
+++ src/js/paginationEvents.js
@@ -1,5 +1,5 @@
 import refsList from './refs';
-import { siteConfigs } from './SiteConfigs';
+import { siteConfigs } from './siteConfigs';
 
 import { fetchTrendingMovies, fetchMovieByQuery } from './fetchMovies';
 import { renderList } from './renderFilmList';
@@ -9,20 +9,20 @@ refsList().rightArrow.addEventListener('click', rightBtnClick);
 refsList().leftArrow.addEventListener('click', leftBtnClick);
 
 function leftBtnClick() {
-//   if (siteConfigs.lastFetch === 'WATCHED')
-//     siteConfigs.decrementWatchedPage();
-//   if (siteConfigs.lastFetch === 'QUEUE')
-//     siteConfigs.decrementQueuePage();
+  //   if (siteConfigs.lastFetch === 'WATCHED')
+  //     siteConfigs.decrementWatchedPage();
+  //   if (siteConfigs.lastFetch === 'QUEUE')
+  //     siteConfigs.decrementQueuePage();
 
   siteConfigs.decrementPage();
   loadMovies();
 }
 
 function rightBtnClick() {
-//   if (siteConfigs.lastFetch === 'WATCHED')
-//     siteConfigs.incrementWatchedPage();
-//   if (siteConfigs.lastFetch === 'QUEUE')
-//     siteConfigs.incrementQueuePage();
+  //   if (siteConfigs.lastFetch === 'WATCHED')
+  //     siteConfigs.incrementWatchedPage();
+  //   if (siteConfigs.lastFetch === 'QUEUE')
+  //     siteConfigs.incrementQueuePage();
   siteConfigs.incrementPage();
 
   loadMovies();
@@ -31,7 +31,7 @@ function rightBtnClick() {
 function getNewPage(e) {
   e.preventDefault();
   if (e.target.nodeName !== 'BUTTON') return;
-    console.log(e.target.dataset.number);
+  console.log(e.target.dataset.number);
   if (e.target.dataset.number !== '0') {
     const page = Number(e.target.dataset.number);
     // if (siteConfigs.lastFetch === 'WATCHED')
@@ -45,12 +45,11 @@ function getNewPage(e) {
 }
 
 async function loadMovies() {
-
   window.scrollTo({
     top: 0,
     left: 0,
   });
-  
+
   if (siteConfigs.lastFetch === 'TRENDING') {
     try {
       const data = await fetchTrendingMovies(siteConfigs.page);
@@ -60,27 +59,29 @@ async function loadMovies() {
     }
   }
 
-  if (siteConfigs.lastFetch === 'SEARCH') {        
+  if (siteConfigs.lastFetch === 'SEARCH') {
     try {
-      const data = await fetchMovieByQuery(siteConfigs.searchQuery, siteConfigs.page);
+      const data = await fetchMovieByQuery(
+        siteConfigs.searchQuery,
+        siteConfigs.page
+      );
       renderList(data, siteConfigs.page);
     } catch (error) {
       console.log(error);
     }
   }
-//   if (siteConfigs.lastFetch === 'WATCHED') {
-//     try {
-//       const data = await watchedTabClickHandler();
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-//   if (siteConfigs.lastFetch === 'QUEUE') {
-//     try {
-//       const data = await queueTabClickHandler();
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-
+  //   if (siteConfigs.lastFetch === 'WATCHED') {
+  //     try {
+  //       const data = await watchedTabClickHandler();
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  //   if (siteConfigs.lastFetch === 'QUEUE') {
+  //     try {
+  //       const data = await queueTabClickHandler();
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
 }
