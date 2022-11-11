8f82167401da9af2541dbe9a6c9e7c4fec455d64
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-11-11 16:28:14 +0200

    script reconnected

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './js/themeSwitcher';
 import './js/team';
 import './js/libraryFromLocalStorage';
 import './js/createPagination';
-import './js/loader';
 
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
